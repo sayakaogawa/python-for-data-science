1e6cc32c5e3167dbe62a207f9c13bbc9193da771
sayakaogawa <sayaka.a.ogawa@gmail.com>
2021-01-20 13:51:31 -0500

    More Numpy tutorial practice.

--- numpy-practice.py
+++ numpy-practice.py
@@ -1,7 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def array_practice():
+def array_part1():
+    """
+    Looking at different descriptor functions of numpy arrays
+    :return:
+    """
     arr1 = np.array([1, 2, 3, 4, 5])  # np array as list
     print('dtype:', arr1.dtype)  # shows what data type is stored
 
@@ -17,12 +22,42 @@ def array_practice():
     return arr3
 
 
+def array_part2():
+    """
+    Looking at numpy array functions for generating arrays
+    :return:
+    """
+
+    # generates a 1D array with values 0-4
+    arr1 = np.arange(5)
+
+    # generates a 1D array with values 20-99
+    arr2 = np.arange(20, 100)
+
+    # generates a 1D array with values 1-20 with jumps of 2
+    arr3 = np.arange(1, 21, 2)
+
+    # np.arange(10) returns array of values 0-9.
+    # but the np.random.permutations shuffles the values in random fashion
+    arr4 = np.random.permutation(np.arange(10))
+
+    # takes a given array and reshapes it to a matrix that is 2x5
+    arr5 = arr4.reshape(2, 5)
+
+    # generates an array with 100 values. Each value is between 0 and 1
+    arr6 = np.random.rand(100)
+
+    # to look at the general distribution of the random values, using
+    # matplotlib to plot a histogram
+    print(plt.hist(arr6))
+
+    return arr1
+
+
 def main():
 
-    arr = array_practice()
-    print(arr[1, 1, 2])
-    print(arr.ndim)
-    print(arr.shape)
+    # arr = array_part1()
+    arr2 = array_part2()
 
 
 main()
