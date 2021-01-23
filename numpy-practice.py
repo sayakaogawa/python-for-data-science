f70bf3f6e41d6307e4cc35fcef7f7a25c4fbc83f
sayakaogawa <sayaka.a.ogawa@gmail.com>
2021-01-23 17:43:34 -0500

    examples of array slicing, masking, and broadcasting.

--- numpy-practice.py
+++ numpy-practice.py
@@ -1,8 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+import numpy.linalg as la # lin alg linrary in np has a lot of fxns, e.g., eigen values; important for data science
 
 
-def array_part1():
+def array_descriptor_fxns():
     """
     Looking at different descriptor functions of numpy arrays
     :return:
@@ -22,7 +23,7 @@ def array_part1():
     return arr3
 
 
-def array_part2():
+def generating_arrays():
     """
     Looking at numpy array functions for generating arrays
     :return:
@@ -46,18 +47,99 @@ def array_part2():
 
     # generates an array with 100 values. Each value is between 0 and 1
     arr6 = np.random.rand(100)
+    
+    # .randn returns a sample from the 'standard normal' distribution.
+    arr7 = np.random.randn(100)
 
     # to look at the general distribution of the random values, using
     # matplotlib to plot a histogram
-    print(plt.hist(arr6))
+    # plt.hist(arr6)
+    # plt.hist(arr7)
 
     return arr1
 
 
+def array_slicing():
+    ' array[start:end:step] '
+    a = np.arange(100)
+    
+    b = a[3:10]
+    b[0] = -1200    # this will also update the element in a
+    
+    c = a[3:10].copy()
+    c[0] = 3    # this will not update the element in a
+    
+    # finding the index of -1200 in a
+    idx = np.argwhere(a==-1200)[0][0]   # adding the [][] will get you the idx of the element
+    a[idx] = 3      # resetting idx to element 3
+    
+    # 2D indices
+    d = np.round(10*np.random.rand(5,4))
+    d[1, 2]     # finds element in row idx 1 (row 2)  and column idx 2 (col 3)
+    
+    # access rows/columns of 2D 
+    d[1,:]  # all col values in 2nd row
+    d[:,1]  # all of the row values in 2nd column
+    
+    # access submatrix
+    d[1:3,2:4]  # row 2&3, then of those rows, picks cols 3&4
+    
+    # transpose matrix
+    e = d.T
+    
+    # generate inverse of a matrix.
+    f = np.random.rand(3,3)
+    g = la.inv(f)    
+    
+    # sorting matrices
+    'if you have more than a 2d array, axis can be 2+'
+    f.sort(axis=0)  # sort based on values in column
+    f.sort(axis=1)  # sort based on values in row
+
+    return b
+    
+
+def array_masking():
+    """ array[index_array] is also known as masking. generates a copy
+     different than slicing, which generates a view """
+    
+    a = np.arange(5)
+    b = a[[1, 3, 4]]    # returns index 1, 3, 4
+    c = a[[True, False, True, False, True]] # returns index 0, 2, 4
+    d = a[a < 4] # this type of boolean array returns all elements that are < 4
+    e = a[(a < 4) & (a > 1)] # returns all elements 1 < x < 4]
+
+    """ difference between & vs. and:
+        and operator is used when both sides of and is one object and has one true value
+        (either true or false)
+        & operator is used when left/right side can be arrays and each element of those
+        arrays can be true or false
+    """
+    return e
+
+
+def array_broadcasting():
+    """ allows you to broadcast an integer into a matrix when trying to
+    update all elements of an array by the same factor.
+    """
+    
+    a = np.array([[2,3],[5,9]])
+    b = a + 5 # adds 5 to each element in the array
+    
+    c = np.array([[1],[3]])
+    d = a + c   # this will add the element '1' from c to each element in the 1st row of a, and 3 for 2nd.
+
+    return a
+
+
 def main():
 
-    # arr = array_part1()
-    arr2 = array_part2()
+    # arr1 = array_descriptor_fxns()
+    # arr2 = generating_arrays()
+    # arr3 = array_slicing()
+    # arr4 = array_masking()
+    # arr5 = array_broadcasting()
+
 
 
 main()
